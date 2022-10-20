3f5918e621a4d0354227681e623ff85a333daaaa
Lean2901 <108638408+Lean2901@users.noreply.github.com>
2022-10-20 20:10:18 +0200

    Add files via upload

---
+++ Persistencia/index.js
@@ -0,0 +1,16 @@
+
+const nombre = "Leandro"
+const apellido = "Massola"
+
+const persona = {
+    nombre: nombre,
+    apellido: apellido,
+}
+
+/*sessionStorage.setItem("persona", JSON.stringify({ nombre: nombre, apellido: apellido}))
+
+localStorage.setItem("persona", JSON.stringify({ nombre: nombre, apellido: apellido}))
+
+
+document.cookie = "otraPersona=persona;expires=" + new Date(2023,9,20,22,07,00).toUTCString()
+console.log(document.cookie)*/
